c873d99cfb27609f67a57de4f9fd7b85b6be6799
Jia Qi Poon <jqp18@ic.ac.uk>
2021-04-10 09:22:21 +0800

    Complete test set1 for problem 1

--- round1a/problem1.py
+++ round1a/problem1.py
@@ -3,21 +3,19 @@ tests = int(input())
 def solve():
     n = int(input()) # list length
     line = list(map(lambda x: int(x), input().split()))
-    original = line.copy()
 
     count = 0
-    for i in range(len(line) - 1):
-        j = line.index(min(line[i:]))
-        line = line[:i] + line[i:j+1][::-1] + line[j+1:]
-        count += (j - i + 1)
+    for i in range(1, len(line)):
+        while line[i] <= line[i-1]:
+            count += 1
+            line[i] = line[i] * 10
 
+            if (line[i] - line[i] % 10) == (line[i-1] - line[i-1] % 10) and (line[i-1] % 10 != 9):
+                line[i] = line[i-1] + 1
+
+    print(line)
     return count
 
 for i in range(tests):
     count = solve()
     print("Case #" + str(i+1) + ": " + str(count))
-
-# Reversort(L):
-#   for i := 1 to length(L) - 1
-#     j := position with the minimum value in L between i and length(L), inclusive
-#     Reverse(L[i..j])
